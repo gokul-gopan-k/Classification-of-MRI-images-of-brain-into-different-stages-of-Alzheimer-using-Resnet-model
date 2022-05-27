a4872f1f52e41e177814deb410a7968c59da7aa0
gokul-gopan-k <gokul.gopan.k@gmail.com>
2022-05-27 19:07:50 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+class CONFIG:
+    data_train = "../input/alzheimers-dataset-4-class-of-images/Alzheimer_s Dataset/train"
+    data_test = = "../input/alzheimers-dataset-4-class-of-images/Alzheimer_s Dataset/test"
+    BATCH_SIZE= 32
+    EPOCHS =50
+    img_height= 224
+    img_width = 224
+ 
